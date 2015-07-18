950aaadb963839b2032a0b0dba5ba0a62375daf7
CHENYANG <li.chenyang1990@gmail.com>
2015-07-18 15:53:26 +0800

    before Quiz

---
+++ Week2_R_programming_ForLoopEg2.R
@@ -0,0 +1,12 @@
+x <- c("a", "b","c","D")
+for (i in seq_along(x)) {
+  print(x[i])
+  
+}
+
+count <- 0
+while (count<10) {
+  print(count)
+  count<-count+1
+  
+}
---
+++ Week2_R_programming_FunctionEg.R
@@ -0,0 +1,11 @@
+#The First Function File 
+add2 <- function(x,y)
+{
+  x +y
+}
+
+above10 <- function(x)
+{
+  use <- x > 10
+  x[use]
+}
